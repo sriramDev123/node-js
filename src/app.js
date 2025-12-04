19c096e78dc23ad2ea299ab7e40786df8c831f51
sriram4111 <saisriram101201@gmail.com>
2025-12-04 18:54:22 +0530

    Error Handing

--- src/app.js
+++ src/app.js
@@ -2,28 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const { UserAuth, adminAuth } = require("./middlewares/auth");
-
-//middleware i can handle all the request before sending response
-app.use("/user", UserAuth);
-
-// app.use("/admin", adminAuth);
-
-app.use("/admin/getdata", adminAuth, (req, res) => {
-  res.send("All admin data has been fetched");
-});
-
-app.use("/admin/login", adminAuth, (req, res) => {
-  res.send("Login to admin panel Successful");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Gone went wrong..!");
+  }
 });
 
 app.use("/user/getdata", (req, res) => {
-  res.send("All user data has been fetched");
+  throw new Error("Forced Error");
+  res.send("User Data Accessed");
 });
 
-app.use("/user/delete", (req, res) => {
-  res.send("All Data has been Deleted");
-});
+// app.use("/", (err, req, res, next) => {
+//   if (err) {
+//     res.status(500).send("Something went wrong..!");
+//   }
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
